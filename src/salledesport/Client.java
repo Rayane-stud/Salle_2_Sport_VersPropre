2eb9f9d93bda8f5687583236f201b72c3f3215b7
gabriel-vignes <gabriel.vignes@epfedu.fr>
2026-02-18 12:09:58 +0100

    commit 1

--- src/salledesport/Client.java
+++ src/salledesport/Client.java
@@ -90,12 +90,96 @@ public class Client extends Utilisateur{
         System.out.println("Voici votre prenom : " + client.prenom );
         System.out.println("Voici votre numero de tel : " + client.tel );
         System.out.println("Voici votre adresse : " + client.adresse );
-        System.out.println("Voici votre nom : " + client.nom );
+        System.out.println("Voici votre type d'abonnement : " + client.typeAbonnement );
 
-
-    
     }
-    public void MAJinfoCOmpte(){}
+    
+    
+    
+    public void MAJinfoCOmpte(){
+        
+        Scanner I = new Scanner(System.in);
+        int c = 100;
+        
+        
+        System.out.println("Qu'est ce que vous voulez modifier ? ");
+        while(c != 0){
+        System.out.println("1: mail, 2: Mot de Passe, 3: nom, 4:prenom, 5: numero de tel, 6: adresse, 7: type d'abonnement, 0: sortir ");
+        c = I.nextInt();
+        switch(c) {
+            
+        /*  case 1: // mail
+                System.out.println("Nombre de pages :");
+                int nbPages = scanner.nextInt();
+                scanner.nextLine();
+                ajouter(description, prix, nbExe, numero, nbPages);
+                break;
+                
+            case 2: // mdp
+                System.out.println("Nombre de pages :");
+                nbPages = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Matière :");
+                String matiere = scanner.nextLine();
+                System.out.println("Niveau :");
+                String niveau = scanner.nextLine();
+                ajouter(description, prix, nbExe, numero, nbPages, matiere, niveau);
+                break;
+                
+            case 3: // nom
+                System.out.println("Périodicité (hebdomadaire/mensuel/trimestriel) :");
+                String perio = scanner.nextLine();
+                System.out.println("Date de publication (format yyyy-MM-dd) :");
+                String dateStr = scanner.nextLine();
+                LocalDate datePub = LocalDate.parse(dateStr);
+                ajouter(description, prix, nbExe, numero, perio, datePub);
+                break;
+            
+            case 4: // prenom
+                System.out.println("Périodicité (hebdomadaire/mensuel/trimestriel) :");
+                String perio = scanner.nextLine();
+                System.out.println("Date de publication (format yyyy-MM-dd) :");
+                String dateStr = scanner.nextLine();
+                LocalDate datePub = LocalDate.parse(dateStr);
+                ajouter(description, prix, nbExe, numero, perio, datePub);
+                break;
+            
+            case 5: // tel
+                System.out.println("Nombre de pages :");
+                int nbPages = scanner.nextInt();
+                scanner.nextLine();
+                ajouter(description, prix, nbExe, numero, nbPages);
+                break;
+            
+            case 6: // adresse
+                System.out.println("Nombre de pages :");
+                int nbPages = scanner.nextInt();
+                scanner.nextLine();
+                ajouter(description, prix, nbExe, numero, nbPages);
+                break;
+            
+            case 7: // type d'abonnement
+                System.out.println("Nombre de pages :");
+                int nbPages = scanner.nextInt();
+                scanner.nextLine();
+                ajouter(description, prix, nbExe, numero, nbPages);
+                break;
+            
+            case 0: // sortir
+                System.out.println("Nombre de pages :");
+                int nbPages = scanner.nextInt();
+                scanner.nextLine();
+                ajouter(description, prix, nbExe, numero, nbPages);
+                break;
+                
+            default:
+                System.out.println("Type non valide.");
+                i--; // Réessayer cet article
+                continue;*/
+        }
+    
+        }
+    }
     public void ConsulterCoursFutur(){}
     public void SinscrirCoursFutur(){}
     public void ConsultCoursFutur(){}
