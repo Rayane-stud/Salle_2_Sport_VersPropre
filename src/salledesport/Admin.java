e71688c84782db72f901ff6054e05a5a09269a48
gabriel-vignes <gabriel.vignes@epfedu.fr>
2026-02-22 10:51:50 +0100

    J'ai modifie le consrtucteur de client pour que le type d'abonnement soit sans gestion erreur avec un switch, j'ai fini MAJcompte mais je comprends pas pq il me demande deux fois nom et prenom. Enfin dans les switch je pense qu'il faut mettre une gestion d'erreur mais jsp comment faire, il y avait un default mais ca mem le mettait à chaque fois, modif avec creer cours

--- src/salledesport/Admin.java
+++ src/salledesport/Admin.java
@@ -4,6 +4,8 @@
  */
 package salledesport;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gabri
@@ -25,7 +27,47 @@ public class Admin extends Utilisateur {
     public void consulterListeCoursPasses() {}
     public void consulterListeCoursFuturs() {}
     public void supprimerUnCoursFutur() {}
-    public void creerUnCoursFutur() {}
+    
+    
+    
+    public Cours creerUnCoursFutur() {
+        int id = 0,c;
+        String activite;
+        String dateHeure;
+        String typeCours = "collectif"; // indiv/collectif
+        int nombreDePlaces;
+        Cours B;
+    Scanner S = new Scanner(System.in);
+    Scanner I = new Scanner(System.in);
+
+    
+    System.out.println("Veuillez rentre l'activite : ");
+    activite = S.nextLine();
+    System.out.println("Veuillez rentre la date et l'heure : ");
+    dateHeure = S.nextLine();
+    System.out.println("Veuillez rentre le typ de cours : ");
+    System.out.println("1: individuel , 2: collectif");
+                c = I.nextInt();
+                switch(c) {
+            
+                    case 1: // individuel
+                        typeCours = "individuel";
+                        break;
+                 
+                    case 2: // collectif
+                        typeCours = "collectif";
+                        break;
+                    default:
+                        System.out.println("Type non valide.");    
+                    }
+    System.out.println("Veuillez rentre le nombre de place : ");
+    nombreDePlaces = I.nextInt();
+    B = new Cours(id,activite,dateHeure,typeCours,nombreDePlaces);
+    
+    return B;
+    }
+    
+    
     public void modifierUnCoursFutur() {}
     public void consulterListeCoursParActivite() {}
     public void verifierCoursPlusPopulaires() {}
